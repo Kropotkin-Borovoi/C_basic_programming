3c898eb8477900e0d1721ebee19d7bba47bf5433
Kropotkin-Borovoi <126001013+Kropotkin-Borovoi@users.noreply.github.com>
2023-04-03 19:51:21 +0300

    Update 7-ая.c

--- 7-ая.c
+++ 7-ая.c
@@ -1,60 +1,62 @@
 #include <stdio.h>
 #include <math.h>
 
-int Max (int* DMas, int numb) {//функция по нахождению максимума массива
-	int i, max=0;
-	for (i=1;i<=numb;i++) {
-		if (DMas[i]>max) {
-			max=DMas[i];
+int Max (int *DMas, int numb) {//функция по нахождению максимума массива
+	int i, max = 0;
+	for (i = 1; i <= numb; i++) {
+		if (DMas[i] > max) {
+			max = DMas[i];
 		}
 	}
-	return (max);
+	return max;
 }
 
 int Min (int *DMas, int numb) {//функция по нахождению минимума массива
-	int i=1, min=Max(DMas, numb);
-	for (i=1;i<=numb;i++) {
-		if (DMas[i]<min) {
-			min=DMas[i];
+	int i = 1, min = Max(DMas, numb);
+	for (i = 1; i <= numb; i++) {
+		if (DMas[i] < min) {
+			min = DMas[i];
 		}
 	}
-	return (min);
+	return min;
 }
 
-float Mean (int *DMas, int numb) {//функция по нахождению среднеарифметического значения массива через оператор (*)
+float Mean (int *DMas, int numb) {//функция по нахождению 
+// среднеарифметического значения массива через оператор (*)
 	int i;
-	int* pMas;//ссылка
-	float count=0;
-	for(i=1, pMas=DMas;i<=numb;i++) {//ссылка принимает значение массива
-		count+=*(pMas+i);
+	float count = 0;
+	for(i = 1; i <= numb; i++) {
+		count += DMas[i];
 	}
-	count/=numb;
-	return (count);
+	count /= numb;
+	return count;
 }
 
-float RMS (int *DMas, int numb) {//функция по нахождению среднеквадратичного отклонения в массиве через оператор (*)
+float RMS (int *DMas, int numb) {//функция по нахождению 
+// среднеквадратичного отклонения в массиве через оператор (*)
 	int i;
-	int* pMas;//ссылка
-	float S=0;
-	for (i=1, pMas=DMas;i<=numb;i++) {//ссылка принимает значение массива
-	S+=pow(*(pMas+i)-Mean(DMas, numb),2);
+	float S = 0;
+	for (i = 1; i <= numb; i++) {
+		S += pow(DMas[i] - Mean(DMas, numb), 2);
 	}
-	S/=numb;
-	return(sqrt(S));
+	S /= numb - 1;
+	return sqrt(S);
 }
 
 int main()
 {
-	printf ("This program calculates the maximum, minimum, mean, square deviation in the entered dynamic array\nEnter the array size\n");//приветствие
+	printf ("This program calculates the maximum, minimum, mean,\
+ square deviation in the entered dynamic array\nEnter the array size\n");
 	int array_size, i;
 	scanf ("%d", &array_size);
-	int* DMas = (int*) malloc(array_size*sizeof(int*));//создаём динамический массив
-	for(i=1;i<=array_size;i++) {
+	int* DMas = (int*) malloc(array_size * sizeof(int));
+	for (i = 1; i <= array_size; i++) {
 		printf ("Enter the %d item\n", i);
-		scanf ("%d", &DMas[i]);//вводим значение для каждого элемента массива
+		scanf ("%d", &DMas[i]);
 	}
 	printf ("\n\nMax = %d\nMin = %d\nMean = %f\nRMS = %f\n", \
-	Max(DMas, array_size), Min(DMas, array_size), Mean(DMas, array_size), RMS(DMas, array_size));
+	Max(DMas, array_size), Min(DMas, array_size), Mean(DMas, \
+ array_size), RMS(DMas, array_size));
 	free(DMas);
 	return 0;
 }
