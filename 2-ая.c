60a1d7125cd18695dd53b25b95a09619919c0747
Kropotkin-Borovoi <126001013+Kropotkin-Borovoi@users.noreply.github.com>
2023-04-03 19:52:08 +0300

    Update 2-ая.c

--- 2-ая.c
+++ 2-ая.c
@@ -4,23 +4,24 @@
 float chech_prime_number (int j) {
 	int i, in_int, counter = 0;
 	float in_float;
-	for (i=2;i<j;i++) {
-		in_float=(float)j/i;
-		in_int=j/i;
-		if (in_int==in_float) {
+	for (i = 2; i < j; i++) {
+		in_float = (float)j / i;
+		in_int = j / i;
+		if (in_int == in_float) {
 			counter++;
 		}
 	}
-	return (counter);
+	return counter;
 }
 
 int main() {
 	int Number, j;
-	printf("Put a number:\n");
-	scanf("%d", &Number);
+	printf ("This program displays all the prime numbers up to the one\
+ specified by the user \nPut a number:\n");
+	scanf ("%d", &Number);
 	printf ("\n");
-	for (j=2;j<=Number;j++) {
-		if (chech_prime_number(j)==0) {
+	for (j = 2; j <= Number; j++) {
+		if (chech_prime_number(j) == 0) {
 			printf ("%d\n", j);
 		}
 	}
