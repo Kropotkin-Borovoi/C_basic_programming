3a50179c668e58db146269dc1b5f6708d49c1391
Kropotkin-Borovoi <126001013+Kropotkin-Borovoi@users.noreply.github.com>
2023-04-13 18:24:35 +0300

    Update 9-ая.c

--- 9-ая.c
+++ 9-ая.c
@@ -5,23 +5,27 @@ typedef struct {
 	float Mas[9];
 } Matrix;
 
-void summ (float *Mas3, float *Mas1, float *Mas2) {//функция по сложению
+Matrix summ (float *Mas1, float *Mas2) {//функция по сложению
+	Matrix m3;
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			Mas3[i * 3 + j] = Mas1[i * 3 + j] + Mas2[i * 3 + j];
+			m3.Mas[i * 3 + j] = Mas1[i * 3 + j] + Mas2[i * 3 + j];
 		}
 	}
+	return m3;
 }
 
-void product (float *Mas3, float *Mas1, float *Mas2) {//функция по умножению
+Matrix multiply (float *Mas1, float *Mas2) {//функция по умножению
+	Matrix m3;
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			Mas3[i * 3 + j] = 0;
+			m3.Mas[i * 3 + j] = 0;
 			for (int k = 0; k < 3; k++) {
-				Mas3[i * 3 + j] = Mas1[i * 3 + k] * Mas2[k * 3 + j] + Mas3[i * 3 + j];	 
+				m3.Mas[i * 3 + j] = Mas1[i * 3 + k] * Mas2[k * 3 + j] + m3.Mas[i * 3 + j];	 
 			}
 		}
 	}
+	return m3;
 }
 
 float det (float *Mas) {//функция по поиску определителя матрицы
@@ -35,16 +39,18 @@ float det (float *Mas) {//функция по поиску определите�
 	return (det);
 }
 
-void reverse (float *Mas3, float *Mas1) {//функция по поиску обратной матрицы
-	Mas3[0] = (Mas1[1 * 3 + 1] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 1] * Mas1[1 * 3 + 2]) / det(Mas1);
-	Mas3[1] = (Mas1[0 * 3 + 1] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 1] * Mas1[0 * 3 + 2]) / det(Mas1);
-	Mas3[2] = (Mas1[0 * 3 + 1] * Mas1[1 * 3 + 2] - Mas1[1 * 3 + 1] * Mas1[0 * 3 + 2]) / det(Mas1);
-	Mas3[3] = (Mas1[1 * 3 + 0] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 0] * Mas1[1 * 3 + 2]) / det(Mas1);
-	Mas3[4] = (Mas1[0 * 3 + 0] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 0] * Mas1[0 * 3 + 2]) / det(Mas1);
-	Mas3[5] = (Mas1[0 * 3 + 0] * Mas1[1 * 3 + 2] - Mas1[1 * 3 + 0] * Mas1[0 * 3 + 2]) / det(Mas1);
-	Mas3[6] = (Mas1[1 * 3 + 0] * Mas1[2 * 3 + 1] - Mas1[2 * 3 + 0] * Mas1[1 * 3 + 1]) / det(Mas1);
-	Mas3[7] = (Mas1[0 * 3 + 0] * Mas1[2 * 3 + 1] - Mas1[2 * 3 + 0] * Mas1[0 * 3 + 1]) / det(Mas1);
-	Mas3[8] = (Mas1[0 * 3 + 0] * Mas1[1 * 3 + 1] - Mas1[1 * 3 + 0] * Mas1[0 * 3 + 1]) / det(Mas1);
+Matrix reverse (float *Mas1) {//функция по поиску обратной матрицы
+	Matrix m3;
+	m3.Mas[0] = (Mas1[1 * 3 + 1] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 1] * Mas1[1 * 3 + 2]) / det(Mas1);
+	m3.Mas[1] = (Mas1[0 * 3 + 1] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 1] * Mas1[0 * 3 + 2]) / det(Mas1);
+	m3.Mas[2] = (Mas1[0 * 3 + 1] * Mas1[1 * 3 + 2] - Mas1[1 * 3 + 1] * Mas1[0 * 3 + 2]) / det(Mas1);
+	m3.Mas[3] = (Mas1[1 * 3 + 0] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 0] * Mas1[1 * 3 + 2]) / det(Mas1);
+	m3.Mas[4] = (Mas1[0 * 3 + 0] * Mas1[2 * 3 + 2] - Mas1[2 * 3 + 0] * Mas1[0 * 3 + 2]) / det(Mas1);
+	m3.Mas[5] = (Mas1[0 * 3 + 0] * Mas1[1 * 3 + 2] - Mas1[1 * 3 + 0] * Mas1[0 * 3 + 2]) / det(Mas1);
+	m3.Mas[6] = (Mas1[1 * 3 + 0] * Mas1[2 * 3 + 1] - Mas1[2 * 3 + 0] * Mas1[1 * 3 + 1]) / det(Mas1);
+	m3.Mas[7] = (Mas1[0 * 3 + 0] * Mas1[2 * 3 + 1] - Mas1[2 * 3 + 0] * Mas1[0 * 3 + 1]) / det(Mas1);
+	m3.Mas[8] = (Mas1[0 * 3 + 0] * Mas1[1 * 3 + 1] - Mas1[1 * 3 + 0] * Mas1[0 * 3 + 1]) / det(Mas1);
+	return m3;
 }
 
 float enter_matrix_by_file (FILE *array_input, float *Mas) {
@@ -90,9 +96,9 @@ int main()
 {
 	int path;/*"path" - переменная по выбору способа считывания 
 			   и записи элементов матрицы*/
-	float Mas3 [3][3];
 	Matrix m1;
 	Matrix m2;
+	Matrix mat;
 	printf ("Enter '0'(self) if you want to enter values from the\
 			 terminal or enter '1'(file) if you want to read from\
 			 a file\n");
@@ -125,35 +131,36 @@ int main()
 	output_matrix(m2.Mas);
 	
 	printf("\n\n");
-	summ(*Mas3, m1.Mas, m2.Mas);//вызываем функцию суммирования
-	output_matrix(*Mas3);
+	//summ(*Mas3, m1.Mas, m2.Mas);//вызываем функцию суммирования
+	mat = summ(m1.Mas, m2.Mas);
+	output_matrix(mat.Mas);
 	
 	printf("\n\n");
-	product(*Mas3, m1.Mas, m2.Mas);//вызываем функцию умножения
-	output_matrix(*Mas3);
+	mat = multiply(m1.Mas, m2.Mas);//вызываем функцию умножения
+	output_matrix(mat.Mas);
 		
 	printf("\n\ndet1 = %f\n\ndet2 = %f\n\n", det(m1.Mas), det(m2.Mas));
 	/*вызываем функцию по обнаружению определителя и сразу 
 	  же выводим его значение*/
 	
-	reverse(*Mas3, m1.Mas);/*вызываем функцию по обнаружению обратной
+	mat = reverse(m1.Mas);/*вызываем функцию по обнаружению обратной
 							 матрицы к первой матрице*/
-	output_matrix(*Mas3);
+	output_matrix(mat.Mas);
 	printf("\n\n");
 		
-	reverse(*Mas3, m2.Mas);/*вызываем функцию по обнаружению обратной
-							 матрицы ко второй матрице*/
-	output_matrix(*Mas3);
+	mat = reverse(m2.Mas);/*вызываем функцию по обнаружению обратной
+							матрицы ко второй матрице*/
+	output_matrix(mat.Mas);
 	
 	
 	FILE *array_output = fopen("/home/hello/Рабочий стол/Основы\
 								программирования/array_output.txt", "w");
-	summ(*Mas3, m1.Mas, m2.Mas);//вызываем функцию суммирования
-	output_matrix_in_file(array_output, *Mas3);
+	mat = summ(m1.Mas, m2.Mas);//вызываем функцию суммирования
+	output_matrix_in_file(array_output, mat.Mas);
 	fprintf (array_output, "\n\n");
 		
-	product(*Mas3, m1.Mas, m2.Mas);//вызываем функцию умножения
-	output_matrix_in_file(array_output, *Mas3);
+	mat = multiply(m1.Mas, m2.Mas);//вызываем функцию умножения
+	output_matrix_in_file(array_output, mat.Mas);
 	fprintf (array_output, "\n\n");
 		
 	fprintf (array_output,"\n\ndet1 = %f\n\ndet2 = %f\n\n",\
@@ -162,14 +169,14 @@ int main()
 	  записываем его значение в файл*/
 	fprintf (array_output, "\n\n");
 		
-	reverse(*Mas3, m1.Mas);/*вызываем функцию по обнаружению обратной
+	mat = reverse(m1.Mas);/*вызываем функцию по обнаружению обратной
 							 матрицы к первой матрице*/
-	output_matrix_in_file(array_output, *Mas3);
+	output_matrix_in_file(array_output, mat.Mas);
 	fprintf (array_output, "\n\n");
 		
-	reverse(*Mas3, m2.Mas);/*вызываем функцию по обнаружению обратной
+	mat = reverse(m2.Mas);/*вызываем функцию по обнаружению обратной
 							 матрицы ко второй матрице*/
-	output_matrix_in_file(array_output, *Mas3);
+	output_matrix_in_file(array_output, mat.Mas);
 	fprintf (array_output, "\n\n");
 	fclose(array_output);
 	return 0;
