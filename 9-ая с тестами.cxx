d98a025583268e01bbb42b3cdbe0f7d91811b113
Kropotkin-Borovoi <126001013+Kropotkin-Borovoi@users.noreply.github.com>
2023-04-24 14:46:49 +0300

    Update 9-ая с тестами.cxx

--- 9-ая с тестами.cxx
+++ 9-ая с тестами.cxx
@@ -106,22 +106,14 @@ void output_matrix_in_file (FILE *array_output, Matrix Mas1) {
 	}
 }
 
-void test (Matrix m1, Matrix m2) {
+void test_Mas_summ (Matrix m1, Matrix m2) {
 	Matrix Mas1;
 	Matrix Mas2;
 	Matrix Mas_summ;
-	Matrix Mas_multiply;
-	Matrix Mas1_reverse;
-	Matrix Mas2_reverse;
 	
 	Mas1 = {1, 2, 3, 7, 5, 6, 7, 8, 9};
 	Mas2 = {3, 4, 7, 2, 8, 9, 0, 4, 1};
 	Mas_summ = {4, 6, 10, 9, 13, 15, 7, 12, 10};
-	Mas_multiply = {7, 32, 28, 31, 92, 100, 37, 128, 130};
-	float det1 = 18;
-	float det2 = -36;
-	Mas1_reverse = {-0.1666, -0.3333, -0.1666, 1.1666, -0.6666, -0.8333, 1.1666, -0.3333, -0.5};
-	Mas2_reverse = {0.7777, 0.6666, 0.5555, -0.0555, -0.0833, -0.3611, -0.2222, -0.3333, -0.4444};
 	
 	m1 = Mas1;
 	m2 = Mas2;
@@ -131,14 +123,6 @@ void test (Matrix m1, Matrix m2) {
 	
 	Matrix Mas_summ_;
 	Mas_summ_ = summ(m1, m2);
-	Matrix Mas_multiply_;
-	Mas_multiply_ = multiply(m1, m2);
-	float det1_1 = det(m1);
-	float det2_2 = det(m2);	
-	Matrix Mas1_reverse_;
-	Mas1_reverse_ = reverse(m1);
-	Matrix Mas2_reverse_;
-	Mas2_reverse_ = reverse(m2);
 	
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
@@ -147,28 +131,122 @@ void test (Matrix m1, Matrix m2) {
 				printf("error");
 				exit(1);
 			}
+		}
+	}
+}
+
+void test_Mas_multiply (Matrix m1, Matrix m2){
+	Matrix Mas1;
+	Matrix Mas2;
+	Matrix Mas_multiply;
+	
+	Mas1 = {1, 2, 3, 7, 5, 6, 7, 8, 9};
+	Mas2 = {3, 4, 7, 2, 8, 9, 0, 4, 1};
+	Mas_multiply = {7, 32, 28, 31, 92, 100, 37, 128, 130};
+	
+	m1 = Mas1;
+	m2 = Mas2;
+	
+	initMatrix(m1);
+	initMatrix(m2);
+	
+	Matrix Mas_multiply_;
+	Mas_multiply_ = multiply(m1, m2);
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
 			if(fabs(Mas_multiply.Mas[i * m1.N + j] - Mas_multiply_.Mas[i * m1.N + j]) > pow(10, -4)) {
 				printf ("\nMas_multiply.Mas[%d] == %f\nMas_multiply_.Mas[%d] == %f", i * m1.N + j, Mas_multiply.Mas[i * m1.N + j], i * m1.N + j, Mas_multiply_.Mas[i * m1.N + j]);
 				printf("error");
 				exit(1);
 			}
+		}
+	}
+}
+
+void test_Mas1_reverse (Matrix m1){
+	Matrix Mas1;
+	Matrix Mas1_reverse;
+	
+	Mas1 = {1, 2, 3, 7, 5, 6, 7, 8, 9};
+	Mas1_reverse = {-0.1666, -0.3333, -0.1666, 1.1666, -0.6666, -0.8333, 1.1666, -0.3333, -0.5};
+	
+	m1 = Mas1;
+	
+	initMatrix(m1);
+	
+	Matrix Mas1_reverse_;
+	Mas1_reverse_ = reverse(m1);
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
 			if(fabs(Mas1_reverse.Mas[i * m1.N + j] - Mas1_reverse_.Mas[i * m1.N + j]) > pow(10, -4)) {
 				printf ("\nMas1_reverse.Mas[%d] == %f\nMas1_reverse_.Mas[%d] == %f", i * m1.N + j, Mas1_reverse.Mas[i * m1.N + j], i * m1.N + j, Mas1_reverse_.Mas[i * m1.N + j]);
 				printf("error");
 				exit(1);
 			}
-			if(fabs(Mas2_reverse.Mas[i * m1.N + j] - Mas2_reverse_.Mas[i * m1.N + j]) > pow(10, -4)) {
-				printf ("\nMas2_reverse.Mas[%d] == %f\nMas2_reverse_.Mas[%d] == %f", i * m1.N + j, Mas2_reverse.Mas[i * m1.N + j], i * m1.N + j, Mas2_reverse_.Mas[i * m1.N + j]);
+		}
+	}
+}
+
+void test_Mas2_reverse (Matrix m2){
+	Matrix Mas2;
+	Matrix Mas2_reverse;
+	
+	Mas2 = {3, 4, 7, 2, 8, 9, 0, 4, 1};
+	Mas2_reverse = {0.7777, 0.6666, 0.5555, -0.0555, -0.0833, -0.3611, -0.2222, -0.3333, -0.4444};
+	
+	m2 = Mas2;
+	
+	initMatrix(m2);
+	
+	Matrix Mas2_reverse_;
+	Mas2_reverse_ = reverse(m2);
+	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if(fabs(Mas2_reverse.Mas[i * m2.N + j] - Mas2_reverse_.Mas[i * m2.N + j]) > pow(10, -4)) {
+				printf ("\nMas2_reverse.Mas[%d] == %f\nMas2_reverse_.Mas[%d] == %f", i * m2.N + j, Mas2_reverse.Mas[i * m2.N + j], i * m2.N + j, Mas2_reverse_.Mas[i * m2.N + j]);
 				printf("error");
 				exit(1);
 			}
 		}
 	}
+}
+
+void test_Mas1_det(Matrix m1){
+	Matrix Mas1;
+	
+	Mas1 = {1, 2, 3, 7, 5, 6, 7, 8, 9};
+	
+	float det1 = 18;
+	
+	m1 = Mas1;
+	
+	initMatrix(m1);
+	
+	float det1_1 = det(m1);
+	
 	if (fabs(det1 - det1_1) > pow(10, -4)) { 
 		printf ("\ndet1 == %f\ndet1_1 == %f", det1, det1_1);
 		printf("error");
 		exit(1);
 	}
+}
+
+void test_Mas2_det(Matrix m2){
+	Matrix Mas2;
+	
+	Mas2 = {3, 4, 7, 2, 8, 9, 0, 4, 1};
+	
+	float det2 = -36;
+	
+	m2 = Mas2;
+	
+	initMatrix(m2);
+	
+	float det2_2 = det(m2);
+	
 	if (fabs(det2 - det2_2) > pow(10, -4)) { 
 		printf ("\ndet2 == %f\ndet2_2 == %f", det2, det2_2);
 		printf("error");
@@ -188,7 +266,12 @@ int main()
 	initMatrix (m2);
 	initMatrix (m3);
 	
-	test (m1, m2);
+	test_Mas_summ(m1, m2);
+	test_Mas_multiply(m1, m2);
+	test_Mas1_reverse(m1);
+	test_Mas2_reverse(m2);
+	test_Mas1_det(m1);
+	test_Mas2_det(m2);
 	
 	printf ("Enter '0'(self) if you want to enter values from the\
 			 terminal or enter '1'(file) if you want to read from\
